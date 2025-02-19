7e03d005f70ff86d415d9a09def6a4002f6448b9
Aidar Tokonov <aydarmaratov17@gmail.com>
2025-02-19 23:34:51 +0600

    Added Authentication and Authorization to project
    Connected nestjs libraries like passport, jwt, guard for auth and authorize

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private config: ConfigService,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET') || 'super-secret',
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.userRepository.findOne({
+      where: { id: payload.sub },
+    });
+
+    return plainToInstance(User, user);
+  }
+}
--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -3,40 +3,42 @@
 import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { AppBaseEntity } from '../../common/app-base.entity';
 import { Role } from '../../role/entities/role.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity('users')
 export class User extends AppBaseEntity {
-    @Column({ type: 'varchar', length: 255 })
-    username: string;
+  @Column({ type: 'varchar', length: 255, unique: true })
+  username: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
-    firstname?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  firstname?: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
-    lastname?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  lastname?: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
-    gender?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  gender?: string;
 
-    @Column({ type: 'int', nullable: true })
-    age?: number;
+  @Column({ type: 'int', nullable: true })
+  age?: number;
 
-    @Column({ type: 'varchar', length: 255, unique: true })
-    email: string;
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
-    password_hash?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  @Exclude()
+  password_hash: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true, unique: true })
-    google_id?: string;
+  @Column({ type: 'varchar', length: 255, nullable: true, unique: true })
+  google_id?: string;
 
-    @ManyToOne(() => Role, role => role.users, { eager: true })
-    @JoinColumn({ name: 'role_id' })
-    role: Role;
+  @ManyToOne(() => Role, (role) => role.users, { eager: true })
+  @JoinColumn({ name: 'role_id' })
+  role: Role;
 
-    @Column({ type: 'boolean', default: true })
-    is_active: boolean;
+  @Column({ type: 'boolean', default: true })
+  is_active: boolean;
 
-    @Column({ type: 'boolean', nullable: true })
-    is_social?: boolean;
+  @Column({ type: 'boolean', nullable: true })
+  is_social?: boolean;
 }
