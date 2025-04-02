1cd97340c86b743b7b6b77955d84043edc0df034
Aidar Tokonov <aydarmaratov17@gmail.com>
2025-04-02 09:29:57 +0600

    Добавлен почтовый сервис для рассылки

--- backend/src/event/event.module.ts
+++ backend/src/event/event.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
-import { EventService } from './event.service';
-import { EventController } from './event.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import {Event} from "./entities/event.entity";
-import {Category} from "../category/entities/category.entity";
-import {Order} from "../order/entities/order.entity";
+import { Module } from "@nestjs/common";
+import { EventService } from "./event.service";
+import { EventController } from "./event.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Event } from "./entities/event.entity";
+import { Category } from "../category/entities/category.entity";
+import { Order } from "../order/entities/order.entity";
+import { EmailModule } from "src/email/email.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event,Category,Order])],
+  imports: [TypeOrmModule.forFeature([Event, Category, Order]), EmailModule],
   controllers: [EventController],
   providers: [EventService],
   exports: [TypeOrmModule],
