3f561306d0cd5425c9e536850e614de525db174e
eldos <ulanbekoveldos@gmail.com>
2025-04-02 11:20:51 +0600

    убрал лишние import

--- backend/src/ticket/ticket.service.ts
+++ backend/src/ticket/ticket.service.ts
@@ -1,4 +1,4 @@
-import {BadRequestException, HttpException, Injectable, Logger, NotFoundException} from '@nestjs/common';
+import {BadRequestException, Injectable,  NotFoundException} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 import {Ticket} from "./entities/ticket.entity";
